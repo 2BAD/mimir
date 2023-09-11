fec4482e35463c894823c2120d896a4627527d5c
Oleh Zhmaiev <oleh@miro.com>
2023-09-11 04:12:30 +0200

    [ts] add first implementation

---
+++ source/fs.ts
@@ -0,0 +1,111 @@
+/* eslint-disable no-console */
+import fs from "node:fs";
+import path from "node:path";
+import { isErrnoException, type TranslationsData } from "./types";
+
+/**
+ * Resolves the path to the translations folder based on the provided path.
+ *
+ * @param translationsPath - The translations path.
+ * @returns The resolved folder path.
+ */
+export const resolveFolderPath = (translationsPath: string): string => {
+  // Check if running inside Jenkins runners or local machine
+  // eslint-disable-next-line dot-notation
+  const pwd = process.env["WORKSPACE"] ?? process.env["PWD"] ?? "";
+  return path.resolve(pwd, translationsPath);
+};
+
+/**
+ * Resolves the path for translations file based on the given path and locale.
+ *
+ * @param translationsPath - The base path for translations.
+ * @param locale - The locale to be used for translations.
+ * @returns The resolved path for the translation file.
+ */
+export const resolveFilePath = (
+  translationsPath: string,
+  locale: string
+): string => {
+  const FILE_NAME = `translation.${locale}.json`;
+  const folderPath = resolveFolderPath(translationsPath);
+  return path.resolve(folderPath, FILE_NAME);
+};
+
+/**
+ * Reads translations from a file at the given file path.
+ *
+ * @param translationsPath - The path to the translations file.
+ * @param locale - The locale of the translations.
+ * @returns The translations data.
+ */
+export const readFromFile = (
+  translationsPath: string,
+  locale: string
+): TranslationsData => {
+  let translations: TranslationsData = {};
+  const filePath = resolveFilePath(translationsPath, locale);
+
+  try {
+    const content = fs.readFileSync(filePath, "utf-8");
+    translations = JSON.parse(content) as TranslationsData;
+  } catch (error) {
+    if (isErrnoException(error)) {
+      switch (error.code) {
+        case "ENOENT":
+          console.error(`Translation file not found at path: ${filePath}.`);
+          break;
+        case "EACCES":
+          console.error(`Permission denied while reading file: ${filePath}.`);
+          break;
+        default:
+          console.error(
+            `An unknown error occurred while reading file: ${error.message}`
+          );
+      }
+    }
+  }
+  return translations;
+};
+
+/**
+ * Retrieves the available locales based on files in translations directory.
+ *
+ * @param translationsPath - The path to the translations directory.
+ * @returns An array of available locales.
+ */
+export const getAvailableLocales = (translationsPath: string): string[] => {
+  let locales: string[] = [];
+  const folderPath = resolveFolderPath(translationsPath);
+
+  try {
+    const files = fs.readdirSync(folderPath);
+    locales = files
+      .filter((file) => /^translation\.[^.]+\.json$/.test(file))
+      .map((file) => file.replace(/^translation\.|\.(json)$/g, ""));
+  } catch (error) {
+    if (isErrnoException(error)) {
+      switch (error.code) {
+        case "ENOENT":
+          console.error(`The directory doesn't exist at path: ${folderPath}.`);
+          break;
+        case "EACCES":
+          console.error(
+            `Permission denied while accessing directory: ${folderPath}.`
+          );
+          break;
+        case "ENOTDIR":
+          console.error(
+            `The specified path is not a directory: ${folderPath}.`
+          );
+          break;
+        default:
+          console.error(
+            `An unknown error occurred while reading file: ${error.message}`
+          );
+      }
+    }
+  }
+
+  return locales;
+};
--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,138 @@
-export const quote = 'Where no man has gone before'
+import fs from "fs";
+import path from "path";
+import { getAvailableLocales, readFromFile } from "./fs";
+import { TranslationsData } from "./types";
+import fg from "fast-glob";
+
+const outputPath = "output.json";
+const rootPath = "/Users/oleh.zhmaiev/dev/client";
+
+function findTranslationsPath(locale: string): string[] {
+  return fg.globSync([
+    `${rootPath}/**/translation.${locale}.json`,
+    "!**/node_modules/**",
+  ]);
+}
+
+function findTranslationsFolder(
+  searchKey: string,
+  translationPaths: string[]
+): string {
+  for (const filePath of translationPaths) {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    const translations: TranslationsData = JSON.parse(fileContent);
+    if (translations[searchKey] !== undefined) {
+      const directoryPath = path.dirname(filePath);
+      return directoryPath;
+    }
+  }
+
+  return "";
+}
+
+function getTranslationContent(locale: string, folderPath: string) {
+  return readFromFile(folderPath, locale);
+}
+
+function getTranslationValue(
+  translationContent: TranslationsData,
+  key: string
+): string {
+  return translationContent[key];
+}
+
+// CHECKS
+function containsHtmlTags(value: string): boolean {
+  return /<[^>]*>/i.test(value);
+}
+
+function containsApostrophes(value: string): boolean {
+  return /'/g.test(value);
+}
+
+function containsPlaceholders(value: string): boolean {
+  return /\{\{.*?\}\}/g.test(value);
+}
+
+function containsPlural(key: string): boolean {
+  return /plural/i.test(key);
+}
+
+// TODO Implement function 'isValueTheLongest"
+function isValueLonger(targetValue: string, value: string): boolean {
+  console.log(targetValue.length, "VS", value.length);
+  return targetValue.length > value.length;
+}
+
+export function getHardKeys(
+  keys: string[],
+  targetLocale: string
+): TranslationsData {
+  const result: TranslationsData = {};
+
+  const translationPaths = findTranslationsPath(targetLocale);
+
+  for (const key of keys) {
+    const folderPath = findTranslationsFolder(key, translationPaths);
+
+    const targetTranslationContent = getTranslationContent(
+      targetLocale,
+      folderPath
+    );
+    const targetValue = getTranslationValue(targetTranslationContent, key);
+    const availableLocales = getAvailableLocales(folderPath);
+
+    if (folderPath && targetValue) {
+      if (
+        containsPlural(key) ||
+        containsHtmlTags(targetValue) ||
+        containsApostrophes(targetValue) ||
+        containsPlaceholders(targetValue)
+      ) {
+        const targetTranslation = readFromFile(folderPath, targetLocale);
+        result[key] = targetTranslation[key];
+      }
+
+      // for (const locale of availableLocales) {
+      //   if (locale !== targetLocale) {
+      //     const translationContent = getTranslationContent(locale, folderPath);
+      //     const translationValue = getTranslationValue(translationContent, key);
+
+      //     console.log(targetValue);
+
+      //     if (isValueLonger(targetValue, translationValue)) {
+      //       const targetTranslation = readFromFile(folderPath, targetLocale);
+      //       result[key] = targetTranslation[key];
+      //     }
+      //   }
+      // }
+    }
+  }
+  return result;
+}
+
+function putHardKeysIntoFile(data: TranslationsData): void {
+  const resultJSON = JSON.stringify(data, null, 2);
+  fs.writeFileSync(outputPath, resultJSON, "utf8");
+}
+
+const keys2: string[] = [
+  "settings.accountSettings.installedApps.addAppsHTML",
+  "settings.accountSettings.installedApps.addedApps",
+  "settings.accountSettings.installedApps.allowNonAdminsToAdd",
+  "settings.accountSettings.installedApps.alreadyAddedApps",
+  "settings.accountSettings.installedApps.appsExtendBoardHTML",
+  "settings.accountSettings.installedApps.askYourAdminToAddApps",
+  "settings.accountSettings.installedApps.authorizedByTeamMember",
+  "settings.accountSettings.installedApps.authorizedByTeamMember_plural",
+  "settings.accountSettings.installedApps.getMoreAppControl",
+  "settings.accountSettings.installedApps.manageAppsInTeam",
+  "settings.accountSettings.installedApps.useAppsToExtendBoard",
+  "settings.accountSettings.platformApps.support.rtbSupportApps.askForSupportHTML",
+  "settings.accountsSettings.editApps.permissions.scopeDescription.auditlogsRead",
+];
+
+const targetLocale = "pt_BR";
+const hardKeys = getHardKeys(keys2, targetLocale);
+
+putHardKeysIntoFile(hardKeys);
