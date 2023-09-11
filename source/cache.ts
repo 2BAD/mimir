1e1f9528a12c5043eb9641f1fa6ba4136a94376a
Oleh Zhmaiev <oleh@miro.com>
2023-09-11 18:13:02 +0200

    [docs] add jsdoc to cache and utils methods

--- source/cache.ts
+++ source/cache.ts
@@ -3,15 +3,18 @@ import { getTranslationContent } from './fs.ts'
 import { type Locale, type LocaleTranslations, type Translations, type TranslationsCache } from './types.ts'
 
 /**
+ * Creates a cache storage for translations.
  *
- * @param translationsPath
+ * @param translationsPath - An array of paths to translation files.
+ * @returns - The translations cache object.
  */
 export const createCacheStorage = (translationsPath: string[]): TranslationsCache => {
   let cache: LocaleTranslations
 
   /**
+   * Refreshes the cache for a specific locale.
    *
-   * @param locale
+   * @param locale - The locale to refresh.
    */
   const refresh = (locale: Locale): void => {
     for (const path of filterPathsByLocale(translationsPath, locale)) {
@@ -20,11 +23,14 @@ export const createCacheStorage = (translationsPath: string[]): TranslationsCach
   }
 
   /**
+   * Gets the translations for a specific locale from the cache.
+   * If the translations are not available in the cache, it refreshes the cache first.
    *
-   * @param locale
+   * @param locale - The locale to retrieve translations for.
+   * @returns - The translations for the specified locale, or null if not found.
    */
   const get = (locale: Locale): Translations | null => {
-    if (!cache[locale]) {
+    if (cache[locale] === undefined) {
       refresh(locale)
     }
     return cache[locale] ?? null
--- source/utils.ts
+++ source/utils.ts
@@ -1,9 +1,11 @@
 import { type Locale } from './types.ts'
 
 /**
+ * Filters an array of paths to keep only those matching a given locale.
  *
- * @param paths
- * @param locale
+ * @param paths - An array of paths to filter.
+ * @param locale - The locale to match against (e.g., 'en', 'fr', 'es').
+ * @returns - The filtered array of paths that match the locale.
  */
 export const filterPathsByLocale = (paths: string[], locale: Locale): string[] => {
   const regex = new RegExp(`^translation\\.${locale}\\.json$`)
