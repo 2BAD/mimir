00f9c186f24b7e1e0735a020ff0dcab450ff8d87
Oleh Zhmaiev <oleh@miro.com>
2023-09-12 22:27:58 +0200

    [misc] remove fs methods that are no longer needed

--- source/fs.ts
+++ source/fs.ts
@@ -16,32 +16,6 @@ export const findTranslationFiles = (searchPath: string, locales: Locale[] = [])
 }
 
 /**
- * Resolves the path to the translations folder based on the provided path.
- *
- * @param translationsPath - The translations path.
- * @returns The resolved folder path.
- */
-export const resolveFolderPath = (translationsPath: string): string => {
-  // Check if running inside Jenkins runners or local machine
-  // eslint-disable-next-line dot-notation
-  const pwd = process.env['WORKSPACE'] ?? process.env['PWD'] ?? ''
-  return path.resolve(pwd, translationsPath)
-}
-
-/**
- * Resolves the path for translations file based on the given path and locale.
- *
- * @param translationsPath - The base path for translations.
- * @param locale - The locale to be used for translations.
- * @returns The resolved path for the translation file.
- */
-export const resolveFilePath = (translationsPath: string, locale: string): string => {
-  const FILE_NAME = `translation.${locale}.json`
-  const folderPath = resolveFolderPath(translationsPath)
-  return path.resolve(folderPath, FILE_NAME)
-}
-
-/**
  * Reads translations from a file at the given file path.
  *
  * @param filePath - The path to the translations file.
@@ -68,39 +42,4 @@ export const getTranslationContent = (filePath: string): Translations => {
     }
   }
   return translations
-}
-
-/**
- * Retrieves the available locales based on files in translations directory.
- *
- * @param folderPath - The path to the translations directory.
- * @returns An array of available locales.
- */
-export const getAvailableLocales = (folderPath: string): Locale[] => {
-  let locales: string[] = []
-
-  try {
-    const files = fs.readdirSync(folderPath)
-    locales = files
-      .filter((file) => /^translation\.[^.]+\.json$/.test(file))
-      .map((file) => file.replace(/^translation\.|\.(json)$/g, ''))
-  } catch (error) {
-    if (isErrnoException(error)) {
-      switch (error.code) {
-        case 'ENOENT':
-          console.error(`The directory doesn't exist at path: ${folderPath}.`)
-          break
-        case 'EACCES':
-          console.error(`Permission denied while accessing directory: ${folderPath}.`)
-          break
-        case 'ENOTDIR':
-          console.error(`The specified path is not a directory: ${folderPath}.`)
-          break
-        default:
-          console.error(`An unknown error occurred while reading file: ${error.message}`)
-      }
-    }
-  }
-
-  return locales as Locale[]
 }
