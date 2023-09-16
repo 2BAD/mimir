c834a2da601ba64e45184d87cd727647675328db
Oleh Zhmaiev <oleh@miro.com>
2023-09-16 04:17:09 +0200

    [ts] add translate commands to `cli`

--- source/cli.ts
+++ source/cli.ts
@@ -2,25 +2,48 @@
 /* eslint-disable vitest/require-hook */
 // eslint-disable-next-line n/shebang
 import meow from 'meow'
+import { initTranslator } from './translator.js'
+import { type Locale } from './types.js'
 
 const cli = meow(
   `
   Usage
-    $ mimir check <locale> <keys>
+  $ mimir translate -p <path> -k <key>
+  $ mimir translate -p <path> -k <key> -l <locale?>
+
+  $ mimir validate -p <path>
+  $ mimir validate -p <path> -k <key>
+  $ mimir validate -p <path> -f <file>
 
   Options
-    --project, -p   Path to project
+    --path, -p     Path to translation folder
+    --key, -k      A key to translate
+    --locale, -l   Comma separated list of locales to process (eg: en,de)
+    --file, -f     Path to files that contains keys to validate
     --output,  -o   Path to output file (result.json by default)
 
   Examples
-    $ mimir check fr "settings.title"
+    $ mimir validate -p /home/client/ -k "settings.title"
 `,
   {
     importMeta: import.meta,
     flags: {
-      project: {
+      path: {
         type: 'string',
-        shortFlag: 'p'
+        shortFlag: 'p',
+        isRequired: true
+      },
+      key: {
+        type: 'string',
+        shortFlag: 'k'
+      },
+      locale: {
+        type: 'string',
+        shortFlag: 'l'
+      },
+      file: {
+        type: 'string',
+        shortFlag: 'f'
       },
       output: {
         type: 'string',
@@ -30,6 +53,16 @@ const cli = meow(
   }
 )
 
-if (cli.input.at(0) === 'check') {
-  console.log('Checking!', cli.flags)
+if (cli.input.at(0) === 'translate') {
+  console.log('Translating!', cli.flags)
+  const translator = initTranslator(cli.flags.path, cli.flags.locale?.split(',') as Locale[])
+  if (cli.flags.locale !== undefined) {
+    console.log(translator.getText(cli.flags.locale as Locale, cli.flags.key ?? ''))
+  } else if (cli.flags.key !== undefined) {
+    console.log(translator.getTranslations(cli.flags.key))
+  }
+}
+if (cli.input.at(0) === 'validate') {
+  console.log('Validating!', cli.flags)
+  // const translator = initTranslator(cli.flags.path, cli.flags.locale?.split(',') as Locale[])
 }
