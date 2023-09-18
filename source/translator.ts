1364dbf25f7c4365b8fb3fedf4ce604fd53aa619
Oleh Zhmaiev <oleh@miro.com>
2023-09-18 20:29:56 +0200

    [ts] refactor to return object with methods and add getKeys

--- source/translator.ts
+++ source/translator.ts
@@ -19,54 +19,62 @@ export const initTranslator = (path: string, locales: Locale[] = []): Translator
 
   const cache = createCache(translationFiles, locales)
 
-  return {
-    /**
-     * Finds the locale for a given text.
-     *
-     * @param text - The text to search for in the translations.
-     * @returns The locale of the text, or null if not found.
-     */
-    findLocaleByText: (text: string): string | null => {
-      for (const translationMap of cache.values()) {
-        for (const [locale, translation] of Object.entries(translationMap)) {
-          if (translation.includes(text)) return locale
-        }
+  /**
+   * Finds the locale for a given text.
+   *
+   * @param text - The text to search for in the translations.
+   * @returns The locale of the text, or null if not found.
+   */
+  const findLocaleByText = (text: string): string | null => {
+    for (const translationMap of cache.values()) {
+      for (const [locale, translation] of Object.entries(translationMap)) {
+        if (translation.includes(text)) return locale
       }
-      return null
-    },
-
-    /**
-     * Retrieves the translated text for a given locale and key.
-     *
-     * @param locale - The locale of the translation.
-     * @param key - The key of the translation.
-     * @returns The translated text, or null if not found.
-     */
-    getText: (locale: Locale, key: string): string | null => {
-      const translations = cache.get(key, locale)
-      return translations?.[locale] ?? null
-    },
-
-    /**
-     * Retrieves translations for a specific key.
-     *
-     * @param key - The key to lookup translations for.
-     * @returns - The translations for the key, or null if not found.
-     */
-    getTranslations: (key: string): TranslationsCacheObject | null => {
-      const translations = cache.get(key)
-      return translations
-    },
-
-    /**
-     * Finds path to translations file for a given key.
-     *
-     * @param key - The key to search for in the cache.
-     * @returns - The path to the translations file if found, or null otherwise.
-     */
-    findTranslationsFolder(key: string): string | null {
-      const translations = cache.get(key)
-      return translations ? translations.path : null
     }
+    return null
   }
+
+  /**
+   * Retrieves the translated text for a given locale and key.
+   *
+   * @param locale - The locale of the translation.
+   * @param key - The key of the translation.
+   * @returns The translated text, or null if not found.
+   */
+  const getText = (locale: Locale, key: string): string | null => {
+    const translations = cache.get(key, locale)
+    return translations?.[locale] ?? null
+  }
+
+  /**
+   * Retrieves translations for a specific key.
+   *
+   * @param key - The key to lookup translations for.
+   * @returns - The translations for the key, or null if not found.
+   */
+  const getTranslations = (key: string): TranslationsCacheObject | null => {
+    const translations = cache.get(key)
+    return translations
+  }
+
+  /**
+   *
+   */
+  const getKeys = (): string[] => {
+    const keys = [...cache.keys()]
+    return keys
+  }
+
+  /**
+   * Finds path to translations file for a given key.
+   *
+   * @param key - The key to search for in the cache.
+   * @returns - The path to the translations file if found, or null otherwise.
+   */
+  const findTranslationsFolder = (key: string): string | null => {
+    const translations = cache.get(key)
+    return translations ? translations.path : null
+  }
+
+  return { getText, getKeys, getTranslations, findLocaleByText, findTranslationsFolder }
 }
