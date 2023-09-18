ad9ca6d1fb218a02c326c4c95a4d3943406e9aa2
Oleh Zhmaiev <oleh@miro.com>
2023-09-18 20:46:09 +0200

    [ts] get all keys for validator if not specified otherwise

--- source/validator.ts
+++ source/validator.ts
@@ -4,15 +4,21 @@ import { type Translator } from '~/types.js'
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const validateTranslations = (
   translator: Translator,
-  keys: string[]
+  keys?: string[]
+  // rules: string[]
   // targetLocale?: string
 ): Array<Record<string, string>> => {
   const errors: Array<Record<string, string>> = []
+
+  if (keys === undefined || keys?.length === 0) {
+    keys = translator.getKeys()
+  }
+
   for (const key of keys) {
     const translations = translator.getTranslations(key)
     if (translations !== null) {
       const result = containsIdenticalTranslation(translations)
-      if (result !== false) errors.push(result)
+      if (result !== false) errors.push({ ...result, key })
       // add some more tests below in a similar fashion
       // ...
     }
