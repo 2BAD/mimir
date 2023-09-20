2abf40fd6c3e5e471be7c6086b542349ecbe3418
Oleh Zhmaiev <oleh@miro.com>
2023-09-20 18:34:58 +0200

    [ts] add rule to list duplicate translations for the same key

---
+++ source/rules/no-duplicate-translations.ts
@@ -0,0 +1,52 @@
+import { type Context, type LifeCycleTriggers, type Rule } from '~/rules/utils/types.js'
+import { type TranslationsMap } from '~/types.js'
+
+const MESSAGE_ID = 'no-duplicate-translations'
+const messages = {
+  [MESSAGE_ID]: 'Key has multiple identical translations.'
+}
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+const create = (context: Context): LifeCycleTriggers => {
+  // eslint-disable-next-line jsdoc/require-jsdoc
+  const onTranslations = (translations: TranslationsMap): void => {
+    const values = Object.values(translations)
+    const fm = values.reduce((m, v) => {
+      if (m.has(v)) {
+        m.set(v, m.get(v) + 1)
+      } else {
+        m.set(v, 1)
+      }
+      return m
+    }, new Map())
+
+    const locales: string[] = Object.entries(translations)
+      .filter(([_, t]) => fm.get(t) > 1)
+      .map((e) => e[0])
+
+    if (locales.length > 0) {
+      context.report({
+        // key,
+        messageId: MESSAGE_ID,
+        // path: translations.path,
+        // translation: translations[locales[0] as Locale] as string,
+        message: `Following locales have the same translation: ${locales.toString()}`
+      })
+    }
+  }
+
+  return {
+    onTranslations
+  }
+}
+
+export const rule: Rule = {
+  create,
+  meta: {
+    type: 'notice',
+    docs: {
+      description: 'Notify if the translations for a specific key are identical.'
+    },
+    messages
+  }
+}
